84f1a1527da498910f8e848f6a6565dab4d0c9a6
Patrick <patricksoeholtpedersen@gmail.com>
2023-03-31 16:13:38 +0200

    Deleted Old Location.
    Signed-off-by: Patrick <patricksoeholtpedersen@gmail.com>

--- src/addons/Patrick/ApiExtension/_output/extension_hint.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ################## THIS IS A GENERATED FILE ##################
-// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
-
-namespace Patrick\ApiExtension\XF\Api\Controller
-{
-	class XFCP_Auth extends \XF\Api\Controller\Auth {}
-}
